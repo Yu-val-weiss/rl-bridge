42cb4dbde22c6f7770006b9fe433ff2fdd2be0e3
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2025-02-20 13:05:03 +0000

    mlp implementation

---
+++ models/mlp.py
@@ -0,0 +1,34 @@
+from collections import OrderedDict
+from typing import Callable
+
+import torch
+from torch import nn
+
+ModuleFactory = Callable[[], nn.Module]
+
+
+class MLP(nn.Module):
+    """Multilayer perceptron"""
+
+    def __init__(
+        self,
+        input_size: int,
+        hidden_size: int,
+        output_size: int,
+        num_layers: int,
+        activation_factory: ModuleFactory = nn.GELU,
+    ):
+        super().__init__()
+        assert num_layers >= 2
+        layers = OrderedDict()
+        layers["fc_in"] = nn.Linear(input_size, hidden_size)
+        layers["activ_in"] = nn.GELU()
+        for i in range(2, num_layers):
+            layers[f"fc_{i}"] = nn.Linear(hidden_size, hidden_size)
+            layers[f"activ_{i}"] = activation_factory()
+        layers["fc_out"] = nn.Linear(hidden_size, output_size)
+
+        self.seq_model = nn.Sequential(layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.seq_model(x)
--- models/policy_network.py
+++ models/policy_network.py
@@ -1,20 +1,17 @@
 import torch
-import torch.nn as nn
+from torch import nn
+
+from models.mlp import MLP
+
 
 class PolicyNetwork(nn.Module):
-
     def __init__(self, output_size, input_size=480, hidden_size=2048):
-        super(PolicyNetwork, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, hidden_size)
-        self.fc4 = nn.Linear(hidden_size, output_size)
-        self.gelu = nn.GELU()
+        super().__init__()
+        # 4 layer multilayer perceptron, with gelu activation and softmax
+        self.mlp = MLP(input_size, hidden_size, output_size, 4)
+
         self.softmax = nn.Softmax(dim=-1)
 
-    def forward(self, x):
-        x = self.gelu(self.fc1(x))
-        x = self.gelu(self.fc2(x))
-        x = self.gelu(self.fc2(x))
-        x = self.fc3(x)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.mlp(x)
         return self.softmax(x)
