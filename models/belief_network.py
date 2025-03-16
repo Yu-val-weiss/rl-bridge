2af02bd57f92efbc84d35af4f79b5df3b196b203
carlabiermann <biermann.carla@gmail.com>
2025-03-16 15:35:18 +0000

    Add dropout layer option and change belief network activation to sigmoid

--- models/belief_network.py
+++ models/belief_network.py
@@ -7,24 +7,30 @@ from .mlp import MLP
 
 
 class BeliefNetwork(nn.Module):
-    def __init__(self, output_size=32, input_size=480, hidden_size=2048):
+    def __init__(
+        self, output_size=24, input_size=84, hidden_size=248, dropout_rate=0.1
+    ):
         super().__init__()
 
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
 
-        # 4 layer multilayer perceptron, with gelu activation and softmax
-        self.mlp = MLP(input_size, hidden_size, output_size, 4)
+        # 4 layer multilayer perceptron, with gelu activation and dropout
+        self.mlp = MLP(
+            input_size,
+            hidden_size,
+            output_size,
+            4,
+            use_dropout=True,
+            dropout_rate=dropout_rate,
+        )
 
-        self.softmax = nn.Softmax(dim=2)
+        self.sigmoid = nn.Sigmoid()
 
         self.to(get_device())
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.mlp(x)
 
-        # Specific to Tiny Bridge, make sure that distribution over cards of 1 player sum to 1.
-        x = x.view(x.size(0), 3, 8)
-
-        return self.softmax(x)
+        return self.sigmoid(x)
