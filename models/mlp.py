9020802deb03fd05132725bf8b768f21902ac9ee
carlabiermann <biermann.carla@gmail.com>
2025-03-16 15:35:18 +0000

    Add dropout layer option and change belief network activation to sigmoid

--- models/mlp.py
+++ models/mlp.py
@@ -18,15 +18,21 @@ class MLP(nn.Module):
         num_layers: int,
         *,
         activation_factory: ModuleFactory = nn.GELU,
+        use_dropout: bool = False,
+        dropout_rate: float = 0.1,
     ):
         super().__init__()
         assert num_layers >= 2
         layers = OrderedDict()
         layers["fc_in"] = nn.Linear(input_size, hidden_size)
         layers["activ_in"] = nn.GELU()
+        if use_dropout:
+            layers["dropout_in"] = nn.Dropout(dropout_rate)
         for i in range(2, num_layers):
             layers[f"fc_{i}"] = nn.Linear(hidden_size, hidden_size)
             layers[f"activ_{i}"] = activation_factory()
+            if use_dropout:
+                layers[f"dropout_{i}"] = nn.Dropout(dropout_rate)
         layers["fc_out"] = nn.Linear(hidden_size, output_size)
 
         self.seq_model = nn.Sequential(layers)
