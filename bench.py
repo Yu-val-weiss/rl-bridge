02ae31c5d07f31bf03ce732b016a2166c33d1dc5
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2025-03-20 18:26:04 +0000

    more graphing

--- bench.py
+++ bench.py
@@ -1,5 +1,10 @@
+import math
+
 import pyperf
+import pyspiel
 import torch
+from open_spiel.python.algorithms.mcts import MCTSBot, RandomRolloutEvaluator
+from open_spiel.python.algorithms.mcts_agent import MCTSAgent
 from open_spiel.python.rl_environment import Environment
 
 from eval.bmcs import BMCS
@@ -13,10 +18,18 @@ if __name__ == "__main__":
     policy_net = PolicyNetwork(9, 84, 2048)
     policy_net.load_state_dict(torch.load("sl/policy_net.pt"))
 
-    env = Environment("tiny_bridge_4p")
+    env = Environment("tiny_bridge_4p", include_full_state=True)
 
     runner = pyperf.Runner()
     for i in range(10, 110, 10):
         ts = env.reset()
         bmcs = BMCS(belief_net, policy_net, [], r_max=i, p_max=i, k=9)
-        runner.timeit(f"BMCS {i}", stmt="bmcs.search(ts)", globals=locals())
+        bot = MCTSBot(
+            pyspiel.load_game("tiny_bridge_4p"),
+            uct_c=math.sqrt(2),
+            max_simulations=i,
+            evaluator=RandomRolloutEvaluator(i),
+        )
+        agent = MCTSAgent(0, 9, bot)
+        # runner.timeit(f"BMCS {i}", stmt="bmcs.search(ts)", globals=locals())
+        runner.timeit(f"MCTS {i}", stmt="agent.step(ts)", globals=locals())
--- bench_eval/graph_bench.py
+++ bench_eval/graph_bench.py
@@ -23,37 +23,60 @@ BMCS 80: Mean +- std dev: 797 ms +- 63 ms
 BMCS 90: Mean +- std dev: 919 ms +- 51 ms
 BMCS 100: Mean +- std dev: 966 ms +- 114 ms
 """
-
-    res = [
-        [
-            int(a.strip().split(" ")[1])
-            if "BMCS" in a
-            else [float(z.strip()[:-3]) for z in a.strip().split("+-")]
-            for a in x.split(":")
-            if "BMCS" in a or "ms" in a
-        ]
-        for x in bench_results.split("\n")
-        if x
-    ]
-
-    xs = [x[0] for x in res]
-    ys = [x[1][0] for x in res]  # type: ignore
-    errs = [x[1][1] for x in res]  # type: ignore
+    bench_results_mcts = """MCTS 10: Mean +- std dev: 4.51 ms +- 0.32 ms
+MCTS 20: Mean +- std dev: 16.6 ms +- 0.4 ms
+MCTS 30: Mean +- std dev: 37.7 ms +- 3.2 ms
+MCTS 40: Mean +- std dev: 63.7 ms +- 5.6 ms
+MCTS 50: Mean +- std dev: 94.9 ms +- 5.1 ms
+MCTS 60: Mean +- std dev: 130 ms +- 16 ms
+MCTS 70: Mean +- std dev: 169 ms +- 27 ms
+MCTS 80: Mean +- std dev: 216 ms +- 58 ms
+MCTS 90: Mean +- std dev: 269 ms +- 63 ms
+MCTS 100: Mean +- std dev: 331 ms +- 88 ms
+"""
 
     fig, ax = plt.subplots()
 
-    ax.errorbar(xs, ys, yerr=errs, fmt=".", capsize=3, elinewidth=0.75)
+    for i, br in enumerate([bench_results, bench_results_mcts]):
+        res = [
+            [
+                int(a.strip().split(" ")[1])
+                if "BMCS" in a or "MCTS" in a
+                else [float(z.strip()[:-3]) for z in a.strip().split("+-")]
+                for a in x.split(":")
+                if "BMCS" in a or "MCTS" in a or "ms" in a
+            ]
+            for x in br.split("\n")
+            if x
+        ]
+
+        xs = [x[0] for x in res]
+        ys = [x[1][0] for x in res]  # type: ignore
+        errs = [x[1][1] for x in res]  # type: ignore
+
+        ax.errorbar(
+            xs,
+            ys,
+            yerr=errs,
+            fmt=".",
+            capsize=3,
+            elinewidth=0.75,
+            label="BMCS" if i == 0 else "MCTS",
+            color="#D81B60" if i == 0 else "#1E88E5",
+        )
 
     ax.set_xticks(range(10, 110, 10))
 
     ax.set_title(
-        r"Benchmarking the BMCS \texttt{search} function", fontdict={"fontsize": 22}
+        r"Benchmarking the \texttt{search} function", fontdict={"fontsize": 22}
     )
 
     ax.tick_params(labelsize=14)
 
     ax.set_ylabel(r"Time per call / ms", fontdict={"size": 18})
-    ax.set_xlabel(r"$\rho = R_{\max} = D_{\max}$", fontdict={"size": 18})
+    ax.set_xlabel(r"$\rho$", fontdict={"size": 18})
+
+    ax.legend(prop={"size": 14})
 
     fig.tight_layout()
 
